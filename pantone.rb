3e2c57b87c9919cc47f596018d421c4d25e6a367
Grant Hutchins <ghutchins@pivotal.io>
2019-10-05 20:09:09 -0500

    Generate 32x32 PNGs

--- pantone.rb
+++ pantone.rb
@@ -5,6 +5,8 @@ require 'pry'
 require 'chunky_png'
 require 'fileutils'
 
+IMAGE_SIZE = [32, 32]
+
 json_data = File.read("./pantone.json")
 data = JSON.parse(json_data)
 colors = data["data"]["getBook"]["colors"]
@@ -15,6 +17,6 @@ FileUtils.mkdir_p("images")
 colors.each do |color|
   filename = "#{color["positionInBook"]}.png"
   rgb = color["rgb"]
-  png = ChunkyPNG::Image.new(16, 16, ChunkyPNG::Color.rgb(rgb["r"], rgb["g"], rgb["b"]))
+  png = ChunkyPNG::Image.new(*IMAGE_SIZE, ChunkyPNG::Color.rgb(rgb["r"], rgb["g"], rgb["b"]))
   png.save("images/#{filename}", :interlace => true)
 end
