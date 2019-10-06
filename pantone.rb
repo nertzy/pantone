733dafc787360528b9363af860e3aac380a15b1e
Grant Hutchins <ghutchins@pivotal.io>
2019-10-05 20:22:47 -0500

    Write CSV data

--- pantone.rb
+++ pantone.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'pry'
 require 'chunky_png'
 require 'fileutils'
+require 'csv'
 
 IMAGE_SIZE = [32, 32]
 
@@ -11,12 +12,20 @@ json_data = File.read("./pantone.json")
 data = JSON.parse(json_data)
 colors = data["data"]["getBook"]["colors"]
 
-FileUtils.rm_rf("images")
-FileUtils.mkdir_p("images")
+FileUtils.rm_rf("out")
+FileUtils.mkdir_p("out/images")
 
-colors.each do |color|
-  filename = "#{color["positionInBook"]}.png"
-  rgb = color["rgb"]
-  png = ChunkyPNG::Image.new(*IMAGE_SIZE, ChunkyPNG::Color.rgb(rgb["r"], rgb["g"], rgb["b"]))
-  png.save("images/#{filename}", :interlace => true)
+CSV.open("out/data.csv", "wb") do |csv|
+  csv << ["Filename", "Code", "Name"]
+  colors.each do |color|
+    filename = "#{color["positionInBook"]}.png"
+    code = color["code"]
+    name = color["name"]
+
+    csv << [filename, code, name]
+
+    rgb = color["rgb"]
+    png = ChunkyPNG::Image.new(*IMAGE_SIZE, ChunkyPNG::Color.rgb(rgb["r"], rgb["g"], rgb["b"]))
+    png.save("out/images/#{filename}", :interlace => true)
+  end
 end
